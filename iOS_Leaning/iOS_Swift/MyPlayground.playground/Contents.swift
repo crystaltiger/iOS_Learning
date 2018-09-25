79a3d9da07b6f81388d0d8c6653ac806d789b220
Jeff Guo <jiliang.guo@qq.com>
2018-09-25 22:24:30 +0800

    add swift leaning

--- iOS_Leaning/iOS_Swift/MyPlayground.playground/Contents.swift
+++ iOS_Leaning/iOS_Swift/MyPlayground.playground/Contents.swift
@@ -2,4 +2,16 @@
 
 import UIKit
 
-var str = "Hello, playground"
+var str = "I am A new swift"
+let m : Int = 10
+var y : Int = 10
+var x  = y + m
+str.uppercased()
+str.lowercased()
+//str.characters.count
+var message = "The number is $" + String(x)
+
+
+let containView = UIView(frame : CGRect(x: 0, y: 0, width: 100, height: 100))
+containView.backgroundColor = UIColor.red
+
